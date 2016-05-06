8f0d01a4e57008d56f90d996b7736c7d9c42d988
Mamisoa Lenovo <nokia100hz@hotmail.com>
2016-05-06 21:52:31 +0200

    finished setting diplay for worklist in home.html

--- models/db1.py
+++ models/db1.py
@@ -1,5 +1,16 @@
+db.define_table('facility',
+    Field('facility_name', 'string', required=True),
+    auth.signature,
+    format='%(facility_name)s')
+
+db.define_table('modality',
+    Field('modality_name', 'string', required=True),
+    auth.signature,
+    format='%(modality_name)s')
+
 db.define_table('address',
     Field('id_auth_user', 'reference auth_user', writable = False, readable = False),
+    Field('id_facility', 'reference facility'),
     Field('home_num_pid11_1', 'integer', required=True),
     Field('box_num_pid11_2', 'string'),
     Field('address1_pid11_3', 'string', required=True),
@@ -12,6 +23,7 @@ db.define_table('address',
 
 db.define_table('phone',
     Field('id_auth_user', 'reference auth_user', writable= False, readable= False),
+    Field('id_facility', 'reference facility'),
     Field('phone_prefix_pid13_1', 'integer', required=True, default = '32'),
     Field('phone_pid13_2', 'string', required=True),
     Field('phone_origin', 'string', required=True ),
@@ -55,12 +67,12 @@ db.define_table('worklist',
     Field('sending_app_MSH3','string', default = 'Oph2Py'),
     Field('sending_facility_MSH4','string', default = 'Eye Center'),
     Field('receving_app_MSH5','string', default = 'Receving App'),
-    Field('receving_facility_MSH6','string', default = 'Eye Center'),
+    Field('receving_facility_MSH6','reference facility', default = '1'),
     Field('message_unique_id_MSH10','string', default=str(request.now), required=True),
     Field('exam2do_OBR4', 'reference exam2do_OBR4', required=True),
     Field('provider_OBR16', 'reference auth_user', required=True),
     Field('requested_time_OBR6', 'datetime', required=True),
-    Field('modality_dest_OBR24', 'string'),
+    Field('modality_dest_OBR24', 'reference modality', required=True),
     Field('status_flag', 'list:string', required=True),
     auth.signature)
 
@@ -74,6 +86,9 @@ db.worklist.id_auth_user.requires=IS_IN_DB(db,'auth_user.id','%(first_name)s'+' 
 db.worklist.provider_OBR16.requires=IS_IN_DB(db(query_sessions), 'auth_user.id', '%(first_name)s'+' '+'%(last_name)s' )
 db.worklist.status_flag.requires=IS_IN_SET(('requested', 'in process', 'done', 'cancelled'))
 db.worklist.exam2do_OBR4.requires=IS_IN_DB(db,'exam2do_OBR4.id', '%(exam_description)s')
+db.worklist.receving_facility_MSH6.requires=IS_IN_DB(db,'facility.id', '%(facility_name)s' + ' ('+'%(id)s)')
+db.worklist.modality_dest_OBR24.requires=IS_IN_DB(db,'modality.id', '%(modality_name)s')
+
 
 db.define_table('test',
     Field('testname','string'),
