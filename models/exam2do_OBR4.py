73eac1e7a3f44d5afde184a7e6b6ab0e2f312a0b
Mamisoa at home <mamisoa@gmail.com>
2016-05-17 01:12:17 +0200

    submit right/left working in rx.html, table and modal to do

--- models/exam2do_OBR4.py
+++ models/exam2do_OBR4.py
@@ -43,16 +43,26 @@ db.define_table('rx',
     Field('id_auth_user','reference auth_user', required=True),
     Field('id_worklist','reference worklist'),
     Field('rx_origin', 'list:string', required=True),
+    Field('glass_type', 'list:string'),
+    Field('va_far','decimal(2,2)'),
+    Field('opto_far','string'),
     Field('sph_far','decimal(2,2)'),
     Field('cyl_far','decimal(2,2)'),
     Field('axis_far', 'integer'),
+    Field('opto_int','string'),
+    Field('va_int','decimal(2,2)'),
     Field('sph_int','decimal(2,2)'),
     Field('cyl_int','decimal(2,2)'),
     Field('axis_int', 'integer'),
+    Field('opto_close','string'),
+    Field('va_close','decimal(2,2)'),
     Field('sph_close','decimal(2,2)'),
     Field('cyl_close','decimal(2,2)'),
     Field('axis_close', 'integer'),
     Field('note','string'),
+    Field('laterality','list:string', required=True),
     auth.signature)
 
 db.rx.rx_origin.requires = IS_IN_SET(('autorx','glass','trial','cyclo','dil'))
+db.rx.laterality.requires = IS_IN_SET(('right','left'))
+db.rx.glass_type.requires = IS_IN_SET(('monofocal','progressive','bifocal','degressive'))
