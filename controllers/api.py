4773556540cd9c596d93478c6f6ce9b82211411f
Mamisoa at home <mamisoa@gmail.com>
2016-07-11 03:05:08 +0200

    building api to get mainTerm and specific results in api.py

--- controllers/api.py
+++ controllers/api.py
@@ -511,21 +511,27 @@ def icd10_index():
     def GET(**vars):
         search_str = request.vars.search
         search_str = search_str.split(' ')
+        xpath_req0 = ['/ICD10CM.index/letter/mainTerm/title[contains(.,"'+search_str[0].capitalize()+'")]']
         xpath_req1 = ['/ICD10CM.index/letter/mainTerm/title[contains(.,"'+search_str[0].capitalize()+'")]']
         for str,val in enumerate(search_str):
             if str == 1: xpath_req1.append('/../term[@level=\"1\"]/title[contains(.,\"' + search_str[1] + '\")')
             elif str > 1: xpath_req1.append(' or contains(.,\"' + search_str[str] + '\")' )
         if len(search_str) >= 2:
-            xpath_req1.append(']/../..')
+            xpath_req1.append(']') # /../..')
         else :
-            xpath_req1.append('/..')
+            xpath_req1.append('')
+        xpath_req0 = ''.join(xpath_req0)
         xpath_req1 = ''.join(xpath_req1)
+        diags0 = icd10_xml.xpathEval(xpath_req0)
         diags1 = icd10_xml.xpathEval(xpath_req1)
         concat = ['<?xml version="1.0" encoding="utf-8"?>\n<main>\n']
-        filter_code = '[A-Z]\d\d\.?[0-9X]?[0-9X]?[0-9X]?[A-Z]?'
+        concat.append('<mainTerm>')
+        for diag0 in diags0:
+            diag0_str = XML(diag0)
+            concat.append(diag0_str + '\n')
         for diag1 in diags1:
             diag1_str = XML(diag1)
             concat.append(diag1_str + '\n')
-        concat.append('</main>')
+        concat.append('</mainTerm></main>')
         return ''.join(concat)
     return locals()
